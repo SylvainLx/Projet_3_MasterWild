12cacb1ca88eef20809ace4d1bb65d38012b0948
OulG <lou.gain@outlook.fr>
2022-06-02 16:03:50 +0200

    Add ChakraProvider in index.jsx and add video in masterclass.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- frontend/src/pages/Masterclass.jsx
+++ frontend/src/pages/Masterclass.jsx
@@ -1,7 +1,15 @@
+import { AspectRatio } from "@chakra-ui/react";
+
 export default function Masterclass() {
   return (
-    <div>
-      <div>Masterclass</div>
+    <div display="flex" align="center">
+      <AspectRatio margin={{ base: "none", md: "40px" }}>
+        <iframe
+          title="Masterclass"
+          src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+          allowFullScreen
+        />
+      </AspectRatio>
     </div>
   );
 }
