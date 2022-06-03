4050778ab2502e5cf0b951481642caa3e01574db
BignonEmma <emmamailpro@gmail.com>
2022-06-03 11:07:17 +0200

    subscribing parts partially done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,18 +5,23 @@ import {
   Container,
   AspectRatio,
 } from "@chakra-ui/react";
-import Harry from "../assets/harryroselmack.jpg";
-import Xavier from "../assets/xavierniel.jpg";
-import LogoBleu from "../assets/logoattente.jpg";
+import Harry from "../assets/harryroselmack.png";
+import Xavier from "../assets/xavierniel.png";
+import LogoBleu from "../assets/logoattente.png";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Container bgColor="#1738CE" height="20%">
         <img src={LogoBleu} alt="inspiraction logo" />
       </Container>
       <div>
-        <Text color="#1738CE" align="center" padding="50px 35px">
+        <Text
+          color="#1738CE"
+          align="center"
+          fontWeight="600"
+          padding="50px 35px"
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1 500s, when an unknown printer took a galley of type and
@@ -84,7 +89,7 @@ export default function Home() {
       </SimpleGrid>
       <div>
         <div display="flex" align="center">
-          <AspectRatio marginTop="7%">
+          <AspectRatio marginTop="5%">
             <iframe
               title="Masterclass"
               src="https://www.youtube.com/embed/QhBnZ6NPOY0"
@@ -94,36 +99,67 @@ export default function Home() {
         </div>
       </div>
       <div>
-        <Text color="#1738CE" align="center" padding="50px 35px">
+        <Text
+          color="#1738CE"
+          align="center"
+          fontWeight="600"
+          padding="50px 35px"
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1 500s, when an unknown printer took a galley of type and
           scrambled it to make.
         </Text>
       </div>
-      <div className="subscribing-area">
-        <div className="basic-sub">
-          <div className="basic-sub-title">
-            <p>Basic</p>
-          </div>
-          <div className="basic-sub-text">
-            <p>Lorem Ipsum is simple dummy text...</p>
-          </div>
-          <div className="subscribe-button">
-            <button type="button">subscribe</button>
-          </div>
-        </div>
-        <div className="premium-sub">
-          <div className="premium-sub-title">
-            <p>Premium</p>
-          </div>
-          <div className="premium-sub-text">
-            <p>Lorem Ipsum is simple dummy text...</p>
-          </div>
-          <div className="subscribe-button">
-            <button type="button">subscribe</button>
-          </div>
-        </div>
+      <div>
+        <SimpleGrid columns={[2]} spacing="0px">
+          <Box bg="#EF4957" height="100%" width="100%" fontSize="1.2rem">
+            <Text
+              color="#1738CE"
+              textAlign="center"
+              marginTop="-25px"
+              fontWeight="600"
+            >
+              <Text as="span" fontSize="4.5rem">
+                B
+              </Text>
+              asic
+            </Text>
+            <Text
+              fontSize="0.8rem"
+              color="white"
+              textAlign="center"
+              padding="15px"
+              marginTop="-20px"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </Text>
+          </Box>
+          <Box bg="#1738CE" height="100%" width="100%" fontSize="1.2rem">
+            <Text
+              color="#EF4957"
+              textAlign="center"
+              marginTop="-25px"
+              fontWeight="600"
+            >
+              <Text as="span" fontSize="4.5rem" letterSpacing="-22px">
+                P
+              </Text>
+              remium
+            </Text>
+            <Text
+              fontSize="0.8rem"
+              color="white"
+              textAlign="center"
+              padding="15px"
+              marginTop="-20px"
+            >
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </Text>
+          </Box>
+        </SimpleGrid>
       </div>
     </div>
   );
