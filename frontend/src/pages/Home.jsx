aa8700626ee670c8cda02bf424d42a5ca2981ef6
BignonEmma <emmamailpro@gmail.com>
2022-06-08 16:25:25 +0200

    added the logo, created the first text, the mosaic, started the subscribing preview

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,77 @@
-// import Harry from "../assets/harryroselmack.png";
-// import Xavier from "../assets/xavierniel.png";
-// import LogoBleu from "../assets/logoattente.png";
+import Harry from "../assets/harryroselmack.png";
+import Xavier from "../assets/xavierniel.png";
+import LogoBleu from "../assets/logoattente.png";
 import "../style/Home.css";
 
 export default function Home() {
   return (
-    <div>
-      <p>Homedgfcfgdf</p>
+    <div className="container-home">
+      <div className="logo-top">
+        <h1>
+          <img src={LogoBleu} alt="logo-inspiraction" />
+        </h1>
+      </div>
+      <div className="intro-text">
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1 500s, when an unknown printer took a galley of type and
+          scrambled it to make.
+        </p>
+      </div>
+      <div className="home-mosaic">
+        <div className="square-one">
+          <div className="text-one">
+            <p>Prénom</p> <p>Nom</p> <p>Métier</p>
+          </div>
+        </div>
+        <div className="square-one-pic">
+          <img src={Xavier} alt="logo-inspiraction" />
+        </div>
+        <div className="square-two-pic">
+          <img src={Harry} alt="logo-inspiraction" />
+        </div>
+        <div className="square-two">
+          <div className="text-two">
+            <p>Prénom</p> <p>Nom</p> <p>Métier</p>
+          </div>
+        </div>
+        <div className="square-three">
+          <div className="text-three">
+            <p>Prénom</p> <p>Nom</p> <p>Métier</p>
+          </div>
+        </div>
+        <div className="square-three-pic">
+          <img src={Xavier} alt="logo-inspiraction" />
+        </div>
+      </div>
+      <div className="video-sample-area">
+        <div className="video-sample">
+          <img src={LogoBleu} alt="logo-inspiraction" />
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1 500s, when an unknown printer took a galley of type
+            and scrambled it to make.
+          </p>
+        </div>
+      </div>
+      <div className="subscribing-area">
+        <h2>2 Offres au choix</h2>
+
+        <div className="subscribe-one">
+          <h3>Vidéo à la demande</h3>
+          <p>
+            5,99€ <span>/ vidéo</span>
+          </p>
+        </div>
+        <div className="subscribe-two">
+          <h3>Abonnement mensuel</h3>
+          <p>
+            14,99€ <span>/ mois</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
