9119c8545a78a5b3eb0f3201539665d90f71b215
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-06-08 11:47:46 +0200

    remove burger menu chakra to npm/css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,85 +1,46 @@
 import { NavLink } from "react-router-dom";
 import "../style/Header.css";
 
-import {
-  Box,
-  Flex,
-  HStack,
-  Link,
-  IconButton,
-  Image,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-} from "@chakra-ui/react";
-
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import { logowhite } from "../assets/logowhite.png";
+import { pushRotate as BurgerMenu } from "react-burger-menu";
+import logowhite from "../assets/logowhite.png";
 
 export default function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
-    <Box bg={useColorModeValue("white", "gray.900")} px={4}>
-      <Flex h={24} alignItems="center" justifyContent="space-between">
-        <IconButton
-          size="lg"
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label="Open Menu"
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={12} alignItems="center">
-          <Box w="auto">
-            <Link href="/">
-              <Image className="logo-header" src={logowhite} alt="logowhite" />
-            </Link>
-          </Box>
-          <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/profil">Profil</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-          </HStack>
-        </HStack>
-      </Flex>
+    <div className="full-header">
+      <BurgerMenu className="mobile-burger">
+        <NavLink className="menu-item" to="/">
+          Home{" "}
+        </NavLink>
+        <NavLink className="menu-item" to="/inscription">
+          Inscription
+        </NavLink>
+        <NavLink className="menu-item" to="/masterclass">
+          Masterclass
+        </NavLink>
+        <NavLink className="menu-item" to="/search">
+          Search
+        </NavLink>
+        <NavLink className="menu-item" to="/paiement">
+          Paiement
+        </NavLink>
+        <NavLink className="menu-item" to="/profil">
+          Profil
+        </NavLink>
+        <NavLink className="menu-item" to="/contact">
+          Contact
+        </NavLink>
+        <NavLink className="menu-item" to="/admin">
+          Admin
+        </NavLink>
+      </BurgerMenu>
 
-      {isOpen ? (
-        <Box
-          position="absolute"
-          zIndex="100"
-          bg="#1738CE"
-          marginTop="1rem"
-          pb={4}
-          display={{ md: "none" }}
-        >
-          <Stack as="nav" spacing={4}>
-            <NavLink className="burgerlink" to="/">
-              Home{" "}
-            </NavLink>
-            <NavLink className="burgerlink" to="/login">
-              Login
-            </NavLink>
-            <NavLink className="burgerlink" to="/masterclass">
-              Masterclass
-            </NavLink>
-            <NavLink className="burgerlink" to="/search">
-              Search
-            </NavLink>
-            <NavLink className="burgerlink" to="/paiement">
-              Paiement
-            </NavLink>
-            <NavLink className="burgerlink" to="/profil">
-              Profil
-            </NavLink>
-            <NavLink className="burgerlink" to="/contact">
-              Contact
-            </NavLink>
-            <NavLink className="burgerlink" to="/admin">
-              Admin
-            </NavLink>
-          </Stack>
-        </Box>
-      ) : null}
-    </Box>
+      <img className="logo-header" src={logowhite} alt="logowhite" />
+
+      <div className="menu-right">
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/profil">Profil</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
+    </div>
   );
 }
