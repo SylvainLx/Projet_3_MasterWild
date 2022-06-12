d0d3153fec85265a5999ac8ae83cc16462e143d1
ClemDSC <clemence.p14@gmail.com>
2022-06-12 20:19:53 +0200

    creation of the page structure

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,22 @@
+import Harry from "../assets/harryroselmack.png";
+
+import "../style/Contact.css";
+
 export default function Contact() {
   return (
     <div>
-      <div>Contact</div>
+      <div className="introContact">
+        <h2 className="titleContact">
+          Des <span className="monoLetter">Q</span>uestions ?
+        </h2>
+      </div>
+      <div className="pageFAQ">
+        <div>
+          <img className="picStar" src={Harry} alt="harry roselmack" />
+        </div>
+        <div className="popularQuestions">popularQuestions</div>
+        <div className="formContact">formulaire de contact</div>
+      </div>
     </div>
   );
 }
