0164822cdf5226028483ce1061a12190bca812a2
ClemDSC <clemence.p14@gmail.com>
2022-06-14 13:47:26 +0200

    install react-select / creation select

--- frontend/src/components/faq-contact/listFAQ.jsx
+++ frontend/src/components/faq-contact/listFAQ.jsx
@@ -1,3 +1,29 @@
+import Select from "react-select";
+
 export default function ListFAQ() {
-  return <div className="popularQuestions">popularQuestions</div>;
+  const options = [
+    { value: "reccurentquestions", label: "Questions fréquentes" },
+    { value: "inspiraction", label: "Qu'est-ce qu'INSPIR/ACTION ?" },
+    { value: "videos", label: "Où regarder les vidéos ?" },
+    { value: "offre", label: "Quel offre choisir ?" },
+    { value: "vanabonnementilla", label: "Comment résilier mon abonnement ?" },
+  ];
+
+  return (
+    <div className="popularQuestions">
+      <Select
+        defaultValue={options[0]}
+        options={options}
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary25: "rgba(239, 73, 87, 0.6)",
+            primary: "#ef4957",
+          },
+        })}
+      />
+    </div>
+  );
 }
