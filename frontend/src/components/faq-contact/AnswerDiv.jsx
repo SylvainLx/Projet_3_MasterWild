be1b3468adc0c4f02a0365d6a85c045bd73243c1
ClemDSC <clemence.p14@gmail.com>
2022-06-15 12:11:08 +0200

    creation select w/ questions and its answers - mobile only

---
+++ frontend/src/components/faq-contact/AnswerDiv.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export default function AnswerDiv({ resp }) {
+  return <div className="answerDiv">{resp}</div>;
+}
--- frontend/src/components/faq-contact/listFAQ.jsx
+++ frontend/src/components/faq-contact/listFAQ.jsx
@@ -1,29 +1,50 @@
-import Select from "react-select";
+import { useState } from "react";
+import AnswerDiv from "./AnswerDiv";
 
 export default function ListFAQ() {
-  const options = [
-    { value: "reccurentquestions", label: "Questions fréquentes" },
-    { value: "inspiraction", label: "Qu'est-ce qu'INSPIR/ACTION ?" },
-    { value: "videos", label: "Où regarder les vidéos ?" },
-    { value: "offre", label: "Quel offre choisir ?" },
-    { value: "vanabonnementilla", label: "Comment résilier mon abonnement ?" },
+  const [selectedOption, setSelectedOption] = useState([]);
+
+  const handleChange = (e) => setSelectedOption(e.target.value);
+
+  const answersFaq = [
+    {
+      id: "1",
+      answer: "INSPIR/ACTION est une plateforme de masterclass.",
+    },
+    {
+      id: "2",
+      answer: "INSPIR/ACTION est une plateforme de masterclass.",
+    },
+    {
+      id: "3",
+      answer: "Les vidéos sont disponibles en ligne sur la plateforme.",
+    },
+    {
+      id: "4",
+      answer:
+        "Vous pouvez choisir d'opter pour de la VOD ou bien un abonnement mensuel donnant accès à l'ensemble des masterclass.",
+    },
+    {
+      id: "5",
+      answer:
+        "Vous pouvez résilier votre abonnement depuis votre espace personnel ou bien en contactant le service client.",
+    },
   ];
 
   return (
     <div className="popularQuestions">
-      <Select
-        defaultValue={options[0]}
-        options={options}
-        theme={(theme) => ({
-          ...theme,
-          borderRadius: 0,
-          colors: {
-            ...theme.colors,
-            primary25: "rgba(239, 73, 87, 0.6)",
-            primary: "#ef4957",
-          },
-        })}
-      />
+      <select className="selectQuestions" onChange={handleChange}>
+        <option value="1">Questions fréquentes</option>
+        <option value="2">Qu'est-ce qu'INSPIR/ACTION ?</option>
+        <option value="3">Où regarder les vidéos ?</option>
+        <option value="4">Quel offre choisir ?</option>
+        <option value="5">Comment résilier mon abonnement ?</option>
+      </select>
+      {answersFaq
+        .filter((resp) => resp.id === selectedOption)
+        .map((resp) => (
+          <AnswerDiv key={resp.id} resp={resp.answer} />
+        ))}
     </div>
   );
 }
