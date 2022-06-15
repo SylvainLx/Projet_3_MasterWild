fbb15c02871fa7a8dd61af884f6d3cc87f49ebd4
OulG <lou.gain@outlook.fr>
2022-06-15 16:09:40 +0200

    Style page paiement - Version mobile

--- frontend/src/pages/Paiement.jsx
+++ frontend/src/pages/Paiement.jsx
@@ -1,72 +1,87 @@
 import "../style/Paiement.css";
+import { NavLink } from "react-router-dom";
+
+import cardLogo from "../assets/card_logo.png";
 
 export default function Paiement() {
   return (
-    <div className="container-paiement">
+    <>
       <div className="header-paiement">
         <h2 className="titleh2">
           Plus qu'une <span className="monoLetter">E</span>tape!
         </h2>
       </div>
-      <div className="abonnement">
-        <h1>Abonnement</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been. Lorem Ipsum is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been.
-        </p>
-      </div>
-      <div>
-        <h2>Payez en ligne</h2>
-        <form className="formPaiement">
-          <label htmlFor="cardName" className="cardName">
-            <input
-              className=""
-              type="text"
-              name="name"
-              id="cardName"
-              placeholder="Nom"
-              required
-            />
-            Nom sur la carte
-          </label>
-          <label htmlFor="cardNumber" className="cardNumber">
-            <input
-              className=""
-              type="number"
-              name="name"
-              id="cardName"
-              placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;"
-              required
-            />
-            N° de carte
-          </label>
-          <div>
-            <label htmlFor="cardNumber" className="cardNumber">
+      <div className="container-paiement">
+        <div className="info-abonnement">
+          <h1 className="abonnement">Abonnement</h1>
+          <p className="text-abonnement">
+            Vous avez le choix entre deux formules. L'une vous permet d'acheter
+            les vidéos de manière individuelle (10€/vidéo). La seconde vous
+            permet d'accèder à toutes les Masterclass de manière illimité
+            (15€/mois). Conditions générales d'abonnement.
+          </p>
+        </div>
+        <div className="form">
+          <h2 className="payez">Payez en ligne</h2>
+          <div className="box-logo">
+            <img className="card-logo" alt="card-logo" src={cardLogo} />
+          </div>
+          <form className="form-paiement">
+            <label htmlFor="cardName" className="card-info">
               <input
-                className=""
+                className="card-input"
+                type="text"
+                name="name"
+                id="cardName"
+                placeholder="Nom"
+                required
+              />
+              NOM APPARAISSANT SUR LA CARTE
+            </label>
+            <label htmlFor="cardNumber" className="card-info">
+              <input
+                className="card-input"
                 type="number"
+                name="name"
+                id="cardName"
+                placeholder="&bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull; &bull;&bull;&bull;&bull;"
+                required
+              />
+              NUMÉRO DE CARTE
+            </label>
+            <label htmlFor="expiration" className="card-info">
+              <input
+                className="card-input"
+                type="text"
                 name="name"
                 id="cardName"
                 placeholder="MM/AA"
                 required
               />
-              Date d'expiration
+              DATE D'EXPIRATION
             </label>
-            <label htmlFor="cardNumber" className="cardNumber">
+            <label htmlFor="crypto" className="card-info">
               <input
-                className=""
+                className="card-input"
                 type="number"
                 name="name"
                 id="cardName"
                 placeholder="&bull;&bull;&bull;"
                 required
               />
-              Cryptogramme visuel
+              CVV
             </label>
-          </div>
-        </form>
+          </form>
+        </div>
+        <button type="submit" className="paiement-button">
+          Paiement
+        </button>
+        <NavLink to="/Profil" className="return-to-profil">
+          <button type="submit" className="button-to-profil">
+            Retour
+          </button>
+        </NavLink>
       </div>
-    </div>
+    </>
   );
 }
