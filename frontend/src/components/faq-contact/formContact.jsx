86d042312f99f4cd4fb3b418c0256e3b045d91ba
ClemDSC <clemence.p14@gmail.com>
2022-06-15 15:52:37 +0200

    page contact ok - temporary form, waiting for formik installation - only mobile version

--- frontend/src/components/faq-contact/formContact.jsx
+++ frontend/src/components/faq-contact/formContact.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+
 export default function FormContact() {
-  return <div className="formContact">formulaire de contact</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="divFormContact">
+      <div className="askContact">
+        <p className="pAsk">Vous n'avez pas trouvé de réponse ?</p>
+        <button className="askButton" type="button" onClick={setOpen}>
+          Nous contacter
+        </button>
+      </div>
+      <form className={open ? "formContact" : "formHidden"} required>
+        <input type="text" className="contactInput" placeholder="Nom" />
+        <input type="text" className="contactInput" placeholder="Prénom" />
+        <input type="text" className="contactInput" placeholder="Email" />
+        <textarea
+          type="text"
+          className="contactInput"
+          placeholder="Saisissez votre demande"
+        />
+        <button type="submit" className="contactButton">
+          Envoyer
+        </button>
+      </form>
+    </div>
+  );
 }
