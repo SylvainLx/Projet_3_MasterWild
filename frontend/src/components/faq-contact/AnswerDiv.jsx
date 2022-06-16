423320985815145510e90b1af3489265279a387c
ClemDSC <clemence.p14@gmail.com>
2022-06-16 19:14:17 +0200

    fix eslint proptypes

--- frontend/src/components/faq-contact/AnswerDiv.jsx
+++ frontend/src/components/faq-contact/AnswerDiv.jsx
@@ -1,4 +1,11 @@
-// eslint-disable-next-line react/prop-types
-export default function AnswerDiv({ resp }) {
+import PropTypes from "prop-types";
+
+function AnswerDiv({ resp }) {
+  AnswerDiv.propTypes = {
+    resp: PropTypes.string.isRequired,
+  };
+
   return <div className="answerDiv">{resp}</div>;
 }
+
+export default AnswerDiv;
