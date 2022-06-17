0d657c1d809b42dcc5cb3b4ac780d85251fb51d0
BignonEmma <emmamailpro@gmail.com>
2022-06-17 10:04:42 +0200

    added an id to create an anchor on the subscribings

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -72,116 +72,146 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="subscribe-mobile">
-        <SubscribeCard />
-      </div>
-      <div className="subscribing-area">
-        <h2>2 Offres au choix</h2>
-        <div className="subscribe-container">
-          <div className="subscribe">
-            <div className="subscribe-flex">
-              <div className="subscribe-title">
-                <h3>Vidéo à la demande</h3>
-                <p>
-                  5,99€ /<span> vidéo</span>
-                </p>
+      <div id="link-subscribe">
+        <div className="subscribe-mobile">
+          <SubscribeCard />
+        </div>
+        <div className="subscribing-area">
+          <h2>2 Offres au choix</h2>
+          <div className="subscribe-container">
+            <div className="subscribe">
+              <div className="subscribe-flex">
+                <div className="subscribe-title">
+                  <h3>Vidéo à la demande</h3>
+                  <p>
+                    5,99€ /<span> vidéo</span>
+                  </p>
+                </div>
+              </div>
+              <div className="options-grid">
+                <div className="box-1">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-2">
+                  <p>option une</p>
+                </div>
+                <div className="box-3">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-4">
+                  <p>option deux</p>
+                </div>
+                <div className="box-5">
+                  <img
+                    className="check-logo"
+                    src={CheckedInactive}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-6">
+                  <p>option trois</p>
+                </div>
+                <div className="box-7">
+                  <img
+                    className="check-logo"
+                    src={CheckedInactive}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-8">
+                  <p>option quatre</p>
+                </div>
+                <div className="box-9">
+                  <img
+                    className="check-logo"
+                    src={CheckedInactive}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-10">
+                  <p>option cinq</p>
+                </div>
+              </div>
+              <div className="flex-button">
+                <button type="button" className="subscribe-button">
+                  Je choisis cette offre
+                </button>
               </div>
             </div>
-            <div className="options-grid">
-              <div className="box-1">
-                <img className="check-logo" src={Checked} alt="logo checked" />
+            <div className="subscribe">
+              <div className="subscribe-flex">
+                <div className="subscribe-title">
+                  <h3>Abonnement mensuel</h3>
+                  <p>
+                    14,99€ /<span> mois</span>
+                  </p>
+                </div>
               </div>
-              <div className="box-2">
-                <p>option une</p>
+              <div className="options-grid">
+                <div className="box-1">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-2">
+                  <p>option une</p>
+                </div>
+                <div className="box-3">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-4">
+                  <p>option deux</p>
+                </div>
+                <div className="box-5">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-6">
+                  <p>option trois</p>
+                </div>
+                <div className="box-7">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-8">
+                  <p>option quatre</p>
+                </div>
+                <div className="box-9">
+                  <img
+                    className="check-logo"
+                    src={Checked}
+                    alt="logo checked"
+                  />
+                </div>
+                <div className="box-10">
+                  <p>option cinq</p>
+                </div>
               </div>
-              <div className="box-3">
-                <img className="check-logo" src={Checked} alt="logo checked" />
+              <div className="flex-button">
+                <button type="button" className="subscribe-button">
+                  Je choisis cette offre
+                </button>
               </div>
-              <div className="box-4">
-                <p>option deux</p>
-              </div>
-              <div className="box-5">
-                <img
-                  className="check-logo"
-                  src={CheckedInactive}
-                  alt="logo checked"
-                />
-              </div>
-              <div className="box-6">
-                <p>option trois</p>
-              </div>
-              <div className="box-7">
-                <img
-                  className="check-logo"
-                  src={CheckedInactive}
-                  alt="logo checked"
-                />
-              </div>
-              <div className="box-8">
-                <p>option quatre</p>
-              </div>
-              <div className="box-9">
-                <img
-                  className="check-logo"
-                  src={CheckedInactive}
-                  alt="logo checked"
-                />
-              </div>
-              <div className="box-10">
-                <p>option cinq</p>
-              </div>
-            </div>
-            <div className="flex-button">
-              <button type="button" className="subscribe-button">
-                Je choisis cette offre
-              </button>
-            </div>
-          </div>
-          <div className="subscribe">
-            <div className="subscribe-flex">
-              <div className="subscribe-title">
-                <h3>Abonnement mensuel</h3>
-                <p>
-                  14,99€ /<span> mois</span>
-                </p>
-              </div>
-            </div>
-            <div className="options-grid">
-              <div className="box-1">
-                <img className="check-logo" src={Checked} alt="logo checked" />
-              </div>
-              <div className="box-2">
-                <p>option une</p>
-              </div>
-              <div className="box-3">
-                <img className="check-logo" src={Checked} alt="logo checked" />
-              </div>
-              <div className="box-4">
-                <p>option deux</p>
-              </div>
-              <div className="box-5">
-                <img className="check-logo" src={Checked} alt="logo checked" />
-              </div>
-              <div className="box-6">
-                <p>option trois</p>
-              </div>
-              <div className="box-7">
-                <img className="check-logo" src={Checked} alt="logo checked" />
-              </div>
-              <div className="box-8">
-                <p>option quatre</p>
-              </div>
-              <div className="box-9">
-                <img className="check-logo" src={Checked} alt="logo checked" />
-              </div>
-              <div className="box-10">
-                <p>option cinq</p>
-              </div>
-            </div>
-            <div className="flex-button">
-              <button type="button" className="subscribe-button">
-                Je choisis cette offre
-              </button>
             </div>
           </div>
         </div>
