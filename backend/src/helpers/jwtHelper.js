9e7bc1f357249df54c2b2a61e9a80479f3926db2
Paul SANCHEZ <paul.sanchez.44@gmail.com>
2022-07-05 10:35:43 +0200

    pull dev

--- backend/src/helpers/jwtHelper.js
+++ backend/src/helpers/jwtHelper.js
@@ -1,11 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-// Creation du token
 const encodeJWT = (payload) => {
   return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: "1h" });
 };
 
-// Decoder le token
 const decodeJWT = (token) => {
   return jwt.decode(token, process.env.TOKEN_SECRET);
 };
