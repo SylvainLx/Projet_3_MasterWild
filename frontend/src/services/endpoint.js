a285ddad5b1f4611c023b20be38d3f96ac7b8198
Loris <45939615+lchastanet@users.noreply.github.com>
2022-07-06 17:04:53 +0200

    Update endpoint.js

--- frontend/src/services/endpoint.js
+++ frontend/src/services/endpoint.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_URL,
-  baseURL: "http://localhost:5001/",
+  baseURL: import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5001/",
   withCredentials: true,
 });
 
