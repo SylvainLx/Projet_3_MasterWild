7e7657e4af68700b08ecfd888395b0f641a9fa42
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-07-08 23:02:56 +0200

    added emailjs, working on it

--- frontend/src/components/faq-contact/formContact.jsx
+++ frontend/src/components/faq-contact/formContact.jsx
@@ -1,7 +1,34 @@
 import { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function FormContact() {
   const [open, setOpen] = useState(false);
+  const [entreprise, setEntreprise] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const postContact = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_58kecic",
+        "template_8vvb84j",
+        {
+          entreprises: entreprise,
+          lastname: lastName,
+          firstname: firstName,
+          mail: email,
+          messages: message,
+        }.current,
+        "VdOV_IwA2lbDCrvNf"
+      )
+      .then((response) => {
+        console.log(response.text, "ok cest bon");
+      });
+  };
 
   return (
     <div className="divFormContact">
@@ -15,19 +42,41 @@ export default function FormContact() {
           Contactez-nous !
         </button>
       </div>
-      <form className={open ? "formContact" : "formHidden"} required>
+      <form
+        className={open ? "formContact" : "formHidden"}
+        required
+        onSubmit={postContact}
+        method="post"
+      >
         <input
           type="text"
           className="contactInput"
           placeholder="Nom de l'entreprise"
+          onChange={(e) => setEntreprise(e.target.value)}
         />
-        <input type="text" className="contactInput" placeholder="Nom" />
-        <input type="text" className="contactInput" placeholder="Prénom" />
-        <input type="text" className="contactInput" placeholder="Email" />
+        <input
+          type="text"
+          className="contactInput"
+          placeholder="Nom"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <input
+          type="text"
+          className="contactInput"
+          placeholder="Prénom"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          className="contactInput"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <textarea
           type="text"
           className="contactInput"
           placeholder="C'est à vous !"
+          onChange={(e) => setMessage(e.target.value)}
         />
         <button type="submit" className="contactButton">
           Envoyer
