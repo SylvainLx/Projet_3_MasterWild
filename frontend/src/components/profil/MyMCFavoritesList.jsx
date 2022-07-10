50761a062f5170e1f8d067ed273ccabb741bd189
ClemDSC <clemence.p14@gmail.com>
2022-07-10 11:42:23 +0200

    delete dead code

--- frontend/src/components/profil/MyMCFavoritesList.jsx
+++ frontend/src/components/profil/MyMCFavoritesList.jsx
@@ -1,31 +1,31 @@
-// /* eslint-disable camelcase */
-// import { useState, useEffect } from "react";
-// import VideoSample from "../VideoSample";
-// import "../../style/MyMCFavoritesList.css";
-// import "../../style/VideoSample.css";
-// import api from "../../services/endpoint";
+/* eslint-disable camelcase */
+import { useState, useEffect } from "react";
+import VideoSample from "../VideoSample";
+import "../../style/MyMCFavoritesList.css";
+import "../../style/VideoSample.css";
+import api from "../../services/endpoint";
 
-// const userId = 1;
+const userId = 1;
 
-// export default function MyMCFavoritesList() {
-//   const [favoriteList, setFavoriteList] = useState([]);
+export default function MyMCFavoritesList() {
+  const [favoriteList, setFavoriteList] = useState([]);
 
-//   useEffect(() => {
-//     api.get(`/api/favorite/${userId}`).then((res) => {
-//       if (res.data.length !== 0) {
-//         setFavoriteList(res.data);
-//       }
-//     });
-//   }, []);
+  useEffect(() => {
+    api.get(`/api/favorite/${userId}`).then((res) => {
+      if (res.data.length !== 0) {
+        setFavoriteList(res.data);
+      }
+    });
+  }, []);
 
-//   return (
-//     <div>
-//       {favoriteList.map(({ masterclass_Id }) => (
-//         <VideoSample
-//           masterclassId={masterclass_Id}
-//           key={`${masterclass_Id}_${userId}`}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {favoriteList.map(({ masterclass_Id }) => (
+        <VideoSample
+          masterclassId={masterclass_Id}
+          key={`${masterclass_Id}_${userId}`}
+        />
+      ))}
+    </div>
+  );
+}
