f0f81c5e0c8d6ae1380a8579aa03aabdb483f2d0
ClemDSC <clemence.p14@gmail.com>
2022-07-11 12:12:44 +0200

    card ok

--- frontend/src/components/CardMasterclass.jsx
+++ frontend/src/components/CardMasterclass.jsx
@@ -4,10 +4,14 @@ import "../style/CardMasterclass.css";
 export default function CardMasterclass({ mastercard }) {
   return (
     <div className="card-container">
-      <p className="pCardNameEntreprise">
-        {mastercard.entreprise.name} ( {mastercard.entreprise.speciality} )
-      </p>
       <div className="contenu-card">
+        <div className="pPresEntreprise">
+          <p className="pCardNameEntreprise">{mastercard.entreprise.name}</p>
+          <p className="pCardSpecialityEntreprise">
+            ( {mastercard.entreprise.speciality} )
+          </p>
+        </div>
+
         <img
           src={`${import.meta.env.VITE_BACKEND_URL}/data/uploads/${
             mastercard.entreprise.logo_name
@@ -15,10 +19,10 @@ export default function CardMasterclass({ mastercard }) {
           alt="logo entreprise"
           className="logo2"
         />
-        <div className="title-desc">
-          <h2 className="title-card">{mastercard.title}</h2>
-          <p className="overflow-ellipsis">{mastercard.description}</p>
-        </div>
+      </div>
+      <div className="title-desc">
+        <h2 className="title-card">{mastercard.title}</h2>
+        <p className="overflow-ellipsis">{mastercard.description}</p>
       </div>
       <div className="button-start">
         <button type="button" className="voir-vid">
