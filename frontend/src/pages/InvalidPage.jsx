d21af63b923e8bc562a076585aa42c8aa478c387
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-12-13 22:01:56 +0100

    create 403 page and redirect if not admin

--- frontend/src/pages/InvalidPage.jsx
+++ frontend/src/pages/InvalidPage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+import Oops from "../assets/oops.png";
+
+import "../style/InvalidPage.css";
+
 export default function InvalidPage() {
-  return <div>Sorry bébé t'es pas sur la liste 😎</div>;
+  return (
+    <div className="page403">
+      <div className="imgContainer">
+        <img src={Oops} alt="oops" className="img" />
+      </div>
+      <div className="textContainer">
+        <p>La page que vous essayez d'acceder est restreinte !</p>
+        <p>Redirection automatique vers la page principale...</p>
+      </div>
+    </div>
+  );
 }
